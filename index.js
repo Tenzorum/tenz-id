cc6551a7d332730375e8a63c89b4418e96b96f47
Mark Pereira <markpereira17@gmail.com>
2018-09-18 18:56:21 +1000

    almost there

--- index.js
+++ index.js
@@ -448,5 +448,5 @@ function listenForClicks () {
 
 function confirmationPageTenz() {
   $('#updateh1 > div > div > h1 > span').html(`Congratulations ${localStorage.ensName} ! You have successfully claimed your TENZ-ID: <br/> 🎉${localStorage.ensName}.tenz-id.xyz 🎉`);
-  $('#update2 > div > div >  h3 > span').html(`Although its immutabily stored in the Ethereum Blockchain and can be viewed <a href="https://ropsten.etherscan.io/tx/${localStorage.txHash}" target="_blank"><b><u>HERE</u></b></a>`);
+  $('#update2 > div > div >  h3 > span').html(`👉 It's immutabily stored in the Ethereum Blockchain and can be viewed <a href="https://ropsten.etherscan.io/tx/${localStorage.txHash}" target="_blank"><b><u>HERE</u></b></a>`);
 }
